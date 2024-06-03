79e82660600b7a7a1a37ae27410b68eeb98d5c88
Ankit singh <singhankit0460@gmail.com>
2024-06-03 11:30:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./components/Banner"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+
+    </>
+  )
+}
+
+export default App
